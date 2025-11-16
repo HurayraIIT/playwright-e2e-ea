229d3a878b675577fadd62890f322e65272a93eb
Abu Hurayra <hurayraiit@gmail.com>
2025-11-16 16:21:04 +0600

    feat: initialize Playwright testing framework with configuration and example test
    - Added package.json to manage dependencies and scripts for the project.
    - Created playwright.config.js for configuring Playwright test settings and browser projects.

---
+++ playwright.config.js
@@ -0,0 +1,70 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+import { config } from "dotenv";
+
+config();
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 4 : 4,
+  timeout: 30 * 1000,
+
+  expect: {
+    timeout: 5_000,
+    toMatchSnapshot: { maxDiffPixelRatio: 0.03 },
+    toHaveScreenshot: { maxDiffPixelRatio: 0.03 },
+  },
+  reporter: "html",
+  use: {
+    baseURL: process.env.BASE_URL,
+    testIdAttribute: "data-id",
+
+    //screenshot: "on",
+    trace: "retain-on-failure",
+    video: "retain-on-failure",
+
+    ignoreHTTPSErrors: true,
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+});
+
